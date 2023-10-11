6372b48f91661d06a4139cb000798b136c76782b
Sergey Sharonin <sharonin.dev@gmail.com>
2023-10-11 17:44:24 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-main>
+      <ContentBlock/>
+    </v-main>
+
+    <v-footer app v-bind="localAttrs">
+      <Footer />
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import ContentBlock from './components/ContentBlock';
+import Footer from './components/Footer';
+
+export default {
+  name: 'App',
+
+  components: {
+    ContentBlock,
+    Footer
+  },
+
+  computed: {
+    localAttrs () {
+      const attrs = {}
+
+      attrs.absolute = true
+      attrs.fixed = false
+
+      return attrs
+    },
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
